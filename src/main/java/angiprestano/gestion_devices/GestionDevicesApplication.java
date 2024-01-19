6e627e85b534103cbf54c06940a4a9c6a291fc0e
Angela Prestano <angiprestano99@gmail.com>
2024-01-19 10:54:55 +0100

    Initial commit

---
+++ src/main/java/angiprestano/gestion_devices/GestionDevicesApplication.java
@@ -0,0 +1,13 @@
+package angiprestano.gestion_devices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDevicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDevicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/angiprestano/gestion_devices/GestionDevicesApplicationTests.java
@@ -0,0 +1,13 @@
+package angiprestano.gestion_devices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDevicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
