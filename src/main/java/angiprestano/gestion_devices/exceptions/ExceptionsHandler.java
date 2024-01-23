7d5b015bbe3fdec0aab7a7c017a5b3086b318c2b
Angela Prestano <angiprestano99@gmail.com>
2024-01-23 15:31:36 +0100

    add new dependencies

--- src/main/java/angiprestano/gestion_devices/exceptions/ExceptionsHandler.java
+++ src/main/java/angiprestano/gestion_devices/exceptions/ExceptionsHandler.java
@@ -1,9 +1,29 @@
 package angiprestano.gestion_devices.exceptions;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class ExceptionsHandler {
-
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(org.springframework.http.HttpStatus.NOT_FOUND)
+    public ErrorPayload handleNotFound(NotFoundException ex){
+        return new ErrorPayload(ex.getMessage(), LocalDateTime.now());
+    }
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(org.springframework.http.HttpStatus.BAD_REQUEST)
+    public ErrorPayload handleBadRequest(BadRequestException ex){
+        return new ErrorPayload(ex.getMessage(),LocalDateTime.now());
+    }
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorPayload handleGenericException(Exception ex){
+        ex.printStackTrace();
+        return new ErrorPayload("Error of server, resolve this problem",LocalDateTime.now());
+    }
 }
