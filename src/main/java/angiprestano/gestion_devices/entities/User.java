491e227a50438bdff92a9186441a369fbf675913
Angela Prestano <angiprestano99@gmail.com>
2024-01-24 16:58:57 +0100

    class user

--- src/main/java/angiprestano/gestion_devices/entities/User.java
+++ src/main/java/angiprestano/gestion_devices/entities/User.java
@@ -1,19 +1,23 @@
 package angiprestano.gestion_devices.entities;
 
+import angiprestano.gestion_devices.ENUM.Role;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 
+import java.util.Collection;
 import java.util.UUID;
 @Entity
 @Table(name = "users")
 @Getter
 @ToString
 
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue
     private UUID id;
@@ -22,6 +26,8 @@ public class User {
     private String surname;
     private String email;
     private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     public void setUsername(String username) {
         this.username = username;
@@ -41,5 +47,30 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
     }
 }
